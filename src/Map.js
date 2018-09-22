e2a358b2b3b920746373af04191e0dd73dae0f6a
Jinal.Shah <jinujets1106@gmail.com>
2018-09-22 11:09:30 -0400

    Clean up code

--- src/Map.js
+++ src/Map.js
@@ -11,24 +11,6 @@ import { Motion, spring } from "react-motion";
 import chroma from "chroma-js";
 import geographyObject from "./50m.json";
 
-// const markers = [
-//   {
-//     markerOffset: -25,
-//     name: "Buenos Aires",
-//     coordinates: [-58.3816, -34.6037]
-//   },
-//   { markerOffset: -25, name: "La Paz", coordinates: [-68.1193, -16.4897] },
-//   { markerOffset: 35, name: "Brasilia", coordinates: [-47.8825, -15.7942] },
-//   { markerOffset: 35, name: "Santiago", coordinates: [-70.6693, -33.4489] },
-//   { markerOffset: 35, name: "Bogota", coordinates: [-74.0721, 4.711] },
-//   { markerOffset: 35, name: "Quito", coordinates: [-78.4678, -0.1807] },
-//   { markerOffset: -20, name: "Georgetown", coordinates: [-58.1551, 6.8013] },
-//   { markerOffset: -25, name: "Asuncion", coordinates: [-57.5759, -25.2637] },
-//   { markerOffset: 35, name: "Paramaribo", coordinates: [-55.2038, 5.852] },
-//   { markerOffset: 35, name: "Montevideo", coordinates: [-56.1645, -34.9011] },
-//   { markerOffset: -15, name: "Caracas", coordinates: [-66.9036, 10.4806] }
-// ];
-
 const mapStyles = {
   width: "40%",
   height: "auto"
