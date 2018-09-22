d1a280f9bb9a078e71bb1e6f0e465fde9ce429f3
Kashyap Murali <katchu11@gmail.com>
2018-09-22 11:49:56 -0400

    frontend

--- src/index.js
+++ src/index.js
@@ -3,12 +3,26 @@ import { render } from "react-dom";
 import Map from "./Map";
 import axios from "axios";
 import "./styles.css";
-
+import ControlledPopup from "./Popup.js";
+import Recharts from "recharts";
+// import Warper from "./Warper";
+import Popup from "reactjs-popup";
+import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from "recharts";
+// const data = [
+//       {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+//       {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+//       {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
+//       {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+//       {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+//       {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+//       {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+// ];
 class App extends Component {
   constructor() {
     super();
     this.state = {
       country: "",
+      open: false,
       graph: [],
       years: [],
       refCount: [],
@@ -74,7 +88,7 @@ class App extends Component {
 
 
   async post(){
-    const test = await axios.put("http://4a2cb2d6.ngrok.io/",{"list" : [this.state.country]}).then(async(response) =>{
+    const test = await axios.put("http://e4bee7c3.ngrok.io/",{"list" : [this.state.country]}).then(async(response) =>{
       // console.log(response["data"]["output"]);
       this.setState({api_data: response["data"]["output"]});
       this.setState({years: response["data"]["output"][0]});
@@ -134,9 +148,10 @@ class App extends Component {
           popData={this.state.popData}
           updateCountry = {this.updateCountry}
         />
-
-
+        // {console.log(this.state.graph)}
+        <ControlledPopup data ={this.state.country}/>
       </div>
+
     );
   }
 }
