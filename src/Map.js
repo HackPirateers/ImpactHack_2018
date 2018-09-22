a398e77b8a4b972c0cf023e03b00360249ed66a6
Aditya Patil <aditya.awesome37@gmail.com>
2018-09-22 18:06:29 -0400

    beutify

--- src/Map.js
+++ src/Map.js
@@ -1,6 +1,6 @@
 import React from "react";
 //We used react-simple-maps to display our globe. Link:(https://www.react-simple-maps.io/)
-//In order to create the globe we need a Composable Map,
+//In order to create the globe we used Composable Map,
 //Zoomable Globe, Geographies, Geography, Markers and Marker
 
 import {
@@ -13,7 +13,7 @@ import {
 } from "react-simple-maps";
 
 //This will allow the spinning motion of the globe.
-import { Motion, spring } from "react-motion";
+import {Motion, spring} from "react-motion";
 
 //This allows there to be color for each different country (Visual)
 import chroma from "chroma-js";
@@ -59,73 +59,45 @@ const subregions = [
 ];
 
 //This is the Map const value which contains the main JSX code for the globe.
-const Map = ({ center, csize, markers, popData, updateCountry }) => (
-  <div>
-    {/* //This controls the spinning motion of the globe. */}
+const Map = ({center, csize, markers, popData, updateCountry}) => (<div>
+  {/* //This controls the spinning motion of the globe. */}
 
-    <Motion
-      defaultStyle={{
-        x: center[0],
-        y: center[1]
-      }}
-      style={{
-        x: spring(center[0]),
-        y: spring(center[1])
-      }}
-    >
+  <Motion defaultStyle={{
+      x: center[0],
+      y: center[1]
+    }} style={{
+      x: spring(center[0]),
+      y: spring(center[1])
+    }}>
 
     {/* //This creation the physical globe. */}
-      {({ x, y }) => (
-        <ComposableMap
-          width={500}
-          height={500}
-          projection="orthographic"
-          projectionConfig={{ scale: 220 }}
-          style={mapStyles}
-        >
-          <ZoomableGlobe center={[x, y]} >
-            <circle
-              cx={250}
-              cy={250}
-              r={220}
-              fill="transparent"
-              stroke="#CFD8DC"
-            />
-            <Geographies
-              disableOptimization
-              geography={geographyObject}
-            >
-              {(geos, proj) =>
-                geos.map((geo, i) => (
-                  //updateCountry() send the selected country name and country abreveation
-                  <Geography
-                    key={geo.id + i}
-                    geography={geo}
-                    projection={proj}
-                    onClick={() =>updateCountry(geo.properties.sovereignt,geo.properties.brk_name,geo.properties.adm0_a3)}
-                    style={{
-                      default: {
-                        fill: popData ? "#CFD8DC" :
-                          colorScale[
-                            subregions.indexOf(geo.properties.subregion)
-                          ],
-                        stroke: "#607D8B",
-                        strokeWidth: 0.75,
-                        outline: "none"
-                      }
-                    }}
-                  />
-                ))
-              }
-            </Geographies>
+    {
+      ({x, y}) => (<ComposableMap width={500} height={500} projection="orthographic" projectionConfig={{
+          scale: 220
+        }} style={mapStyles}>
+        <ZoomableGlobe center={[x, y]}>
+          <circle cx={250} cy={250} r={220} fill="transparent" stroke="#CFD8DC"/>
+          <Geographies disableOptimization="disableOptimization" geography={geographyObject}>
+            {
+              (geos, proj) => geos.map((geo, i) => (
+              //updateCountry() send the selected country name and country abreveation
+              <Geography key={geo.id + i} geography={geo} projection={proj} onClick={() => updateCountry(geo.properties.sovereignt, geo.properties.brk_name, geo.properties.adm0_a3)} style={{
+                  default: {
+                    fill: popData
+                      ? "#CFD8DC"
+                      : colorScale[subregions.indexOf(geo.properties.subregion)],
+                    stroke: "#607D8B",
+                    strokeWidth: 0.75,
+                    outline: "none"
+                  }
+                }}/>))
+            }
+          </Geographies>
 
-
-
-          </ZoomableGlobe>
-        </ComposableMap>
-      )}
-    </Motion>
-  </div>
-);
+        </ZoomableGlobe>
+      </ComposableMap>)
+    }
+  </Motion>
+</div>);
 
 export default Map;
